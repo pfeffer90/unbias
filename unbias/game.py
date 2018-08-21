70d5c46e4ad97c95843348a137905c4835392f6f
Paul Pfeiffer <pfeifferpaul90@gmail.com>
2018-08-21 08:01:28 +0100

    game adds a trial

--- unbias/game.py
+++ unbias/game.py
@@ -1,8 +1,22 @@
+import numpy as np
 import pandas as pd
+
+
+class GameConstants:
+    trial_idx = 'TrialIdx'
+    agent_choice = 'AgentChoice'
+    reward = 'Reward'
+
+    agent_data = [trial_idx, agent_choice, reward]
 
 
 class Game:
     def add_trial(self, agent_choice, reward):
+        new_trial = pd.DataFrame(np.array([self.number_of_trials + 1, agent_choice, reward]).reshape(1, 3),
+                                 columns=GameConstants.agent_data)
+
+        self.trials = pd.concat([self.trials, new_trial])
+        self.trials[GameConstants.agent_choice] = self.trials[GameConstants.agent_choice].apply(np.int64)
         self.number_of_trials += 1
 
     def provide_reward(self, agent_choice):
@@ -18,6 +32,5 @@ class Game:
         """
         :param unbiaser: a map from trial history to reward that tries to remove bias and history dependence in the choice of the agent
         """
-        agent_data = ['TrialIdx', 'AgentChoice', 'Reward']
         self.number_of_trials = 0
-        self.trials = pd.DataFrame(columns=agent_data)
+        self.trials = pd.DataFrame(columns=GameConstants.agent_data)
--- unbias/test_game.py
+++ unbias/test_game.py
@@ -1,6 +1,19 @@
-from game import Game
+from game import Game, GameConstants
 
 
 def test_initialize_game():
     unbias_dummy = None
     Game(unbias_dummy)
+
+
+def test_add_trial_adds_a_row_with_the_last_trial_data():
+    unbias_dummy = None
+    agent_choice = 0
+    reward = 1
+
+    game = Game(unbias_dummy)
+    game.add_trial(agent_choice, reward)
+
+    last_trial_data = game.trials.tail(1)
+    assert last_trial_data[GameConstants.agent_choice].values[0] == agent_choice
+    assert last_trial_data[GameConstants.reward].values[0] == reward
