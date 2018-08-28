0cbf38b82945315938ce3d50d7268951b8fc9e69
Paul Pfeiffer <pfeifferpaul90@gmail.com>
2018-08-28 17:10:04 +0100

    fixed generation of training data

--- unbias/training_data.py
+++ unbias/training_data.py
@@ -6,14 +6,14 @@ def sigmoid(w, x):
 
 
 def generate_stationary_agent_choices(number_of_trials, w, initial_choices):
-    agent_choices = np.zeros((number_of_trials, ))
+    agent_choices = np.zeros((number_of_trials,))
     history_length = w.shape[0]
-    agent_choices[1:history_length] = initial_choices
+    agent_choices[:history_length - 1] = initial_choices
     for i in range(number_of_trials - history_length):
         history = np.concatenate([np.array([1]), agent_choices[i:i + history_length - 1]])
         probability_for_one = sigmoid(w, history)
-        agent_choices[i + history_length] = \
-        np.random.choice(AGENT_CHOICES, 1, p=[probability_for_one, 1 - probability_for_one])[0]
+        agent_choices[i + history_length - 1] = \
+            np.random.choice(AGENT_CHOICES, 1, p=[probability_for_one, 1 - probability_for_one])[0]
     return agent_choices
 
 
